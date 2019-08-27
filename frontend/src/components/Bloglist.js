eac3ed5e16738e83a6a3e13f8c05b872efc2ec34
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-26 17:23:00 -0700

    5.1: Bloglist frontend - mapping through blogs includes key

--- frontend/src/components/Bloglist.js
+++ frontend/src/components/Bloglist.js
@@ -4,8 +4,8 @@ import Blog from './Blog'
 
 const Bloglist = ({ blogs }) => {
 
-  const blogsList = () => blogs.map(
-    blog => <Blog blog={blog} />
+  const blogsList = () => blogs.map((blog) => 
+    <Blog key={blog.id} blog={blog} />
   )
 
   return (
