865443e4ae7b1fb574852959882d6c55ad98d4fb
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-27 17:59:20 -0700

    5.5 Bloglist Blog create styling

--- frontend/src/components/CreateBlog.js
+++ frontend/src/components/CreateBlog.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
-const CreateBlog = ({addBlog, author, handleAuthor, title, handleTitle, 
+const CreateBlog = ({ addBlog, author, handleAuthor, title, handleTitle,
   url, handleUrl }) => {
 
   return (
     <form id="create-blog" onSubmit={addBlog}>
       <h2>Add blog</h2>
-      <label htmlFor="title">Title</label>
-      <input value={title} onChange={handleTitle} />
-      <label htmlFor="author">Author</label>
-      <input value={author} onChange={handleAuthor} />
-      <label htmlFor="url">URL</label>
-      <input value={url} onChange={handleUrl} />
+      <div className="blog-input">
+        <label htmlFor="title">Title</label>
+        <input value={title} onChange={handleTitle} />
+      </div>
+      <div className="blog-input">
+        <label htmlFor="author">Author</label>
+        <input value={author} onChange={handleAuthor} />
+      </div>
+      <div className="blog-input">
+        <label htmlFor="url">URL</label>
+        <input value={url} onChange={handleUrl} />
+      </div>
       <button type='submit'>Add</button>
     </form>
   )
