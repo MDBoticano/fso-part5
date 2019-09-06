3cef0cae78f3f545ab21f62450bb894901cbe486
Marlo Boticano <m.boticano@gmail.com>
2019-09-05 18:17:50 -0700

    5.15: SimpleBlog test - clicking button means the passed event handler is called twice

--- frontend/src/components/SimpleBlog.test.js
+++ frontend/src/components/SimpleBlog.test.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
+const blog = {
+  title: 'My Blog Title',
+  author: 'Mun Row',
+  likes: 100,
+}
+
 test('renders content', () => {
-  const blog = {
-    title: 'My Blog Title',
-    author: 'Mun Row',
-    likes: 100,
-  }
 
   const component = render(<SimpleBlog blog={blog} />)
 
@@ -19,3 +20,19 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent('blog has 100 likes')
 })
 
+test('onClick works', () => {
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+
+  const button = getByText('like')
+  
+  fireEvent.click(button)
+  expect(mockHandler.mock.calls.length).toBe(1)
+
+  fireEvent.click(button)
+  expect(mockHandler.mock.calls.length).toBe(2)
+})
